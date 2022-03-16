a96538d80ac0b1f309c85d957a0253f540f3f307
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2022-03-16 22:53:08 +0600

    inventory service

--- InventoryService/main.js
+++ InventoryService/main.js
@@ -1,6 +1,24 @@
-var http = require('http')
+const connectDB = require("./config/db");
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const listenForMessage = require("./listenForMessage");
+const Product = require("./Model/Product");
+app.use(express.json({ extende: false }));
+connectDB()
+listenForMessage();
+app.post("/", async (req, res) => {
+    let { name, availableQty, price } = req.body;
+    let product = new Product({
+        name,
+        availableQty,
+        price,
+    });
+    await product.save();
+    res.json(product);
+});
 
-http.createServer(function (req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end("Hello World")
-}).listen(8080)
+const PORT = 8082
+app.listen(PORT, () => {
+    console.log("Inventory Service On Port: " + PORT)
+})
